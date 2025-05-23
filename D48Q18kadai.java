77f601ac82f31014e83c5d9dd77f5e78e2d47889
s24002 <s24002@std.it-college.ac.jp>
2025-05-23 11:38:45 +0900

    D49Q18kadai

--- D48Q18kadai.java
+++ D48Q18kadai.java
@@ -1,6 +1,9 @@
 public class D48Q18kadai {
     public static void main(String[] args) {
-        int n = 1;
-        System.out.println();
+        int total = 75;
+        int capacity = 30;
+        int cargo = 2;
+
+        System.out.println(capacity*cargo-(cargo*cargo*cargo*cargo+cargo*cargo));
     }
 }
